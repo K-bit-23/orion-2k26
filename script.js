11de24a0550d7cd1c513a35e87bcf976123f83ad
karthikeyan_p <dkarthideepak@gmail.com>
2026-01-21 15:59:56 +0530

    Update Reel Rush themes and other changes

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // --- Countdown Timer ---
     // Target Date: January 30, 2026, 09:00:00 (Assuming start time)
-    const eventDate = new Date('January 30, 2026 09:00:00').getTime();
+    const eventDate = new Date('February 20, 2026 08:00:00').getTime();
 
     function updateCountdown() {
         const now = new Date().getTime();
